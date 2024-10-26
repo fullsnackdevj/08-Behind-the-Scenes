3b3aa1a53abef22a9f6fcf180e8e0df8aa646f75
fullsnackdevj <jayfullsnackdev@gmail.com>
2024-10-26 20:42:03 +0800

    using Object.assign

--- starter/primitives_vs_objects.js
+++ starter/primitives_vs_objects.js
@@ -1,7 +1,37 @@
 'use strict';
 
+//primitives
+
 let lastName = 'Williams';
 let oldLastName = lastName;
 lastName = 'Davis';
 
 console.log(lastName, oldLastName);
+
+// reference ' objects '
+
+const jessica = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const marriedJessica = jessica;
+marriedJessica.lastName = 'Davis';
+console.log('Before marriage:', jessica);
+console.log('After marriage:', marriedJessica);
+
+// marriedJessica = {}; Uncaught TypeError: Assignment to constant variable.
+
+// copying objects
+
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: 'Williams',
+  age: 27,
+};
+
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+console.log('Before marriage:', jessica2);
+console.log('After marriage:', jessicaCopy);
